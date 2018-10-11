f86a6bf650fc8aa84a29b4e8a4614383da75a4bd
Varun Talwar <tallytalwar@gmail.com>
2018-10-11 11:24:49 -0700

    Drop: Updated demo app for testing

--- platforms/ios/demo/src/MapViewController.h
+++ platforms/ios/demo/src/MapViewController.h
@@ -12,12 +12,16 @@
 
 - (void)mapView:(nonnull TGMapView *)mapView didLoadScene:(int)sceneID withError:(nullable NSError *)sceneError;
 - (void)mapViewDidCompleteLoading:(nonnull TGMapView *)mapView;
+- (void)mapViewRegionIsChanging:(TGMapView *)mapView;
+- (void)mapView:(TGMapView *)mapView regionDidChangeAnimated:(BOOL)animated;
+- (void)mapView:(TGMapView *)mapView regionWillChangeAnimated:(BOOL)animated;
 - (void)mapView:(nonnull TGMapView *)mapView didSelectFeature:(nullable NSDictionary *)feature atScreenPosition:(CGPoint)position;
 - (void)mapView:(nonnull TGMapView *)mapView didSelectLabel:(nullable TGLabelPickResult *)labelPickResult atScreenPosition:(CGPoint)position;
 - (void)mapView:(nonnull TGMapView *)mapView didSelectMarker:(nullable TGMarkerPickResult *)markerPickResult atScreenPosition:(CGPoint)position;
 - (void)mapView:(nonnull TGMapView *)view didCaptureScreenshot:(nonnull UIImage *)screenshot;
 
 - (void)mapView:(nonnull TGMapView *)view recognizer:(nonnull UIGestureRecognizer *)recognizer didRecognizeSingleTapGesture:(CGPoint)location;
+- (void)mapView:(TGMapView *)view recognizer:(UIGestureRecognizer *)recognizer didRecognizeDoubleTapGesture:(CGPoint)location;
 - (void)mapView:(nonnull TGMapView *)view recognizer:(nonnull UIGestureRecognizer *)recognizer didRecognizeLongPressGesture:(CGPoint)location;
 
 @end
